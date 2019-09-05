bfc9c5814470ff0d9862167acc3c385c32b41558
blewis49 <jblewis49@me.com>
2019-09-05 05:21:22 -0400

    included cases for 1s and 5s

--- greed.py
+++ greed.py
@@ -4,15 +4,16 @@ def score(dice):
     mini_score = {1:100, 5:50}
     for d in dice:
         die[d] = die.get(d,0) + 1
-        s = [score[k] for k,v in die.items() if die[k] >= 3]
-        # if the number of 1's or 5's < 3:
+    s = [score[k] for k,v in die.items() if die[k] >= 3]
     for d in dice:
-        if d == 1 and die[d] < 3:
-            s.append(100*die[d])
-        if d == 5 and die[d] < 3:
-            s.append(50*die[d])
+        if d not in [1, 5]: continue
+        elif die[d] > 3:
+            s.append((die[d]-3)*mini_score[d])
+        elif die[d] < 3:
+            s.append(die[d]* mini_score[d])
+        else: pass
     return sum(s)
 
 
-result = score([1, 1, 1, 1, 1])
+result = score([2, 4, 4, 5, 4])
 print(result)
