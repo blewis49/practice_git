cc50f5b7f95bc6befd02af39520b358d6374e4e2
blewis49 <jblewis49@me.com>
2019-09-04 22:11:50 -0400

    made additional edits to function

--- greed.py
+++ greed.py
@@ -1,17 +1,18 @@
 def score(dice):
     die = dict()
     score = {1:1000, 2:200, 3:300, 4:400, 5:500, 6:600}
+    mini_score = {1:100, 5:50}
     for d in dice:
         die[d] = die.get(d,0) + 1
-        s = [die[d].get(score[d]) for d in die if die[d] == 3]
-        print(s)
+        s = [score[k] for k,v in die.items() if die[k] >= 3]
         # if the number of 1's or 5's < 3:
-        if die.keys() in [1, 5]:
-            die.get(val, 0)
-        # append to s 100 pts for each occurrence of a 1 in the die list
-        # append to s 50 pts for each occurrence of a 5 in the die list
-    return s
+    for d in dice:
+        if d == 1 and die[d] < 3:
+            s.append(100*die[d])
+        if d == 5 and die[d] < 3:
+            s.append(50*die[d])
+    return sum(s)
 
 
-result = score([4, 4, 4, 3, 3])
+result = score([1, 1, 1, 1, 1])
 print(result)
