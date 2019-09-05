641f6d05b3aab571330f0470a0d26612a3ad114f
blewis49 <jblewis49@me.com>
2019-09-04 20:24:22 -0400

    add another file

---
+++ greed.py
@@ -0,0 +1,14 @@
+def score(dice):
+    die = dict()
+    score = {1:1000, 2:200, 3:300, 4:400, 5:500, 6:600}
+    for d in dice:
+        die[d] = die.get(d,0) + 1
+        s = [die[d].get(score[d]) for d in die if die[d] == 3]
+        # if the number of 1's or 5's < 3:
+        # append to s 100 pts for each occurrence of a 1 in the die list
+        # append to s 50 pts for each occurrence of a 5 in the die list
+    return s
+
+
+result = score([4, 4, 4, 3, 3])
+print(result)
