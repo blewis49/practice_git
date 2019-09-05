eb509175270db73de2260f75506d22970ecc1f85
blewis49 <jblewis49@me.com>
2019-09-04 20:35:02 -0400

    edited code snippet

--- greed.py
+++ greed.py
@@ -4,7 +4,10 @@ def score(dice):
     for d in dice:
         die[d] = die.get(d,0) + 1
         s = [die[d].get(score[d]) for d in die if die[d] == 3]
+        print(s)
         # if the number of 1's or 5's < 3:
+        if die.keys() in [1, 5]:
+            die.get(val, 0)
         # append to s 100 pts for each occurrence of a 1 in the die list
         # append to s 50 pts for each occurrence of a 5 in the die list
     return s
